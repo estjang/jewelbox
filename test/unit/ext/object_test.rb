79c8c87caf8ddaf3f84b20df9648a46f21235739
Steve Jang <estebanjang@gmail.com>
2012-05-20 08:31:47 -0700

    Adding eigenclass unit test

--- test/unit/ext/object_test.rb
+++ test/unit/ext/object_test.rb
@@ -4,11 +4,14 @@ require 'jewelbox/ext/object'
 class MyTestClass
 end
 
-class JewelboxTimeTest < MiniTest::Unit::TestCase
+class JewelboxObjectTest < MiniTest::Unit::TestCase
 
   def test_eigenclass
     ec = MyTestClass.eigenclass
-    puts ec.name
+    ec.send(:define_method, :test1) do |x|
+      return x + x
+    end
+    assert_equal(4, MyTestClass.test1(2))
   end
 
 end
